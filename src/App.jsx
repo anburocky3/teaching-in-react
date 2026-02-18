9a35b6815aeed83a2d8f27d99bf0b9f68c6d7270
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2026-02-18 12:40:22 +0530

    refactor(components): rename ListItem to BadgeItem and update usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import ListItem from "./components/BadgeItem";
+import BadgeItem from "./components/BadgeItem";
 
 function AppPage() {
   return (
@@ -9,12 +9,32 @@ function AppPage() {
       <div className="bg-orange-100 rounded text-black p-20">
         <h1 className="text-4xl text-center">Learning React!</h1>
         <ul className="flex items-center justify-center gap-5 mt-5">
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          <BadgeItem skill="HTML" color="blue">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              className="w-4"
+            >
+              <path
+                fill="currentColor"
+                d="M7 21v-2h4v-3.1q-1.225-.275-2.187-1.037T7.4 12.95q-1.875-.225-3.137-1.637T3 8V7q0-.825.588-1.412T5 5h2V3h10v2h2q.825 0 1.413.588T21 7v1q0 1.9-1.263 3.313T16.6 12.95q-.45 1.15-1.412 1.913T13 15.9V19h4v2zm0-10.2V7H5v1q0 .95.55 1.713T7 10.8m5 3.2q1.25 0 2.125-.875T15 11V5H9v6q0 1.25.875 2.125T12 14m5-3.2q.9-.325 1.45-1.088T19 8V7h-2zm-5-1.3"
+              />
+            </svg>
+          </BadgeItem>
+          <BadgeItem skill="CSS" color="green" />
+          <BadgeItem skill="JavaScript" color="yellow" />
+          <BadgeItem skill="React" color="purple" />
+
+          {/* <BadgeItem skill="HTML" color="#0000FF" />
+          <BadgeItem skill="CSS" color="#00FF00" />
+          <BadgeItem skill="JavaScript" color="#FFFF00" />
+          <BadgeItem skill="React" color="#800080" />
+          <BadgeItem skill="PHP" color="#FFC0CB" /> */}
         </ul>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/components/BadgeItem.jsx
+++ src/components/BadgeItem.jsx
@@ -1,5 +1,27 @@
-const ListItem = () => {
-  return <li className="bg-purple-500 px-4 py-1 text-white rounded">HTML</li>;
+const BadgeItem = (prop) => {
+  console.log(prop);
+  // console.log(prop.skill);
+
+  let badgeColor = "bg-gray-500"; // Default color
+  if (prop.color === "blue") {
+    badgeColor = "bg-blue-500";
+  } else if (prop.color === "green") {
+    badgeColor = "bg-green-500";
+  } else if (prop.color === "yellow") {
+    badgeColor = "bg-yellow-500";
+  } else if (prop.color === "purple") {
+    badgeColor = "bg-purple-500";
+  }
+
+  return (
+    <li
+      className={`${badgeColor} text-white px-4 py-2 rounded flex items-center gap-2`}
+      // style={{ backgroundColor: prop.color }}
+    >
+      {prop.children}
+      {prop.skill}
+    </li>
+  );
 };
 
-export default ListItem;
+export default BadgeItem;
